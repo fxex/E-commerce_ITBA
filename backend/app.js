f640be3958136c8d5f251969649fa3c114563f9d
fxex <hugoantoniofrey2003@gmail.com>
2025-10-02 11:11:24 -0300

    Funcionalidad: Se genero los middleware correspondiente para el manejo del 404 y el manejo de errores centralizado

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const log = require("./middleware/global")
+const errorHandler = require("./middleware/errorHandler")
+const handler404 = require("./middleware/handler404")
 
 const app = express()
 const port = 3000
 
 // Middlewares
-app.use(express.json())
 app.use(log)
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+// Uso del manejador del recurso no encontrado
+app.use(handler404)
+
+// Uso del manejador de errores centralizado
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ backend/middleware/handler404.js
@@ -0,0 +1,7 @@
+const recursoNoEncontrado = (req, res, next) =>{
+    const error = new Error(`Ruta no encontrada: ${req.originalUrl}`)
+    error.status = 404
+    next(error)
+}
+
+module.exports = recursoNoEncontrado
