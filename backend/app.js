697210bb24e6af9ff42213df6a711013a5106791
fxex <hugoantoniofrey2003@gmail.com>
2025-10-02 10:55:11 -0300

    Funcionalidad: Se creo la funcion del middleware y se genero la estructura del backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const log = require("./middleware/global")
 
 const app = express()
 const port = 3000
+
+// Middlewares
+app.use(express.json())
+app.use(log)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
