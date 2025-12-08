7a3b8033c59648239d13438cd1c569fdea9ab6c6
fxex <hugoantoniofrey2003@gmail.com>
2025-12-08 18:34:49 -0300

    se modifico la url a usar

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,2 +1,2 @@
-// export const URL = "https://e-commerce-itba.onrender.com/api";
-export const URL = "http://localhost:3000/api";
+export const URL = "https://e-commerce-itba.onrender.com/api";
+// export const URL = "http://localhost:3000/api";
